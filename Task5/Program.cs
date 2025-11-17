e1eb41c4fbd11fe0d49f6b30bfd55644b223b205
pokhrelameet12@gmail.com <pokhrelameet12@gmail.com>
2025-11-17 16:09:33 +0545

    Completed the enum defination task for task5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace Task5;
-
-class Program
+﻿// Enumm Defination
+public enum DayType
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    Weekday,
+    Weekend,
 }
+// record the defination
+public record Book(string Title, string Author, double Price);
+Console.WriteLine("Task 5 Enums and Records");
+
+//Enum usage
+Console.Write("Enter day: ");
+string dayInput = Console.ReadLine();
+
+//check if weekend or weekday
+DayType daytype = (dayInput.ToLower() == "Friday" || dayInput.ToLower() == "Saturday") ? DayType.Weekday : DayType.Weekend;
+Console.WriteLine($"It is : {daytype}");
+
+//Record usage
+Book book1 = new Book("C# Programming","Kushal Tamang", 29.99);
+
+//Create new record with changes
+Book book2 =book1 with {Title = "Advanced C#", Price =49.99};
+Console.WriteLine($"Book1: {book1}");
+
+//Deconstruct record
+var (title, author, price) = book2;
+Console.WriteLine($"Title: {title}");
+Console.WriteLine($"Author: {author}");
+Console.WriteLine($"Price: {price}");
